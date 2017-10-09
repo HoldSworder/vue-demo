505b15dce9a3ea3d2830edda0cedb84675d62624
qzr <z5021996@vip.qq.com>
2017-10-09 13:43:51 +0800

    vue-demo

---
+++ src/components/apple.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>I am apple</h1>
+</template>
+
+<script>
+    export default {
+        name: 'apple',
+    }
+</script>
+
+
---
+++ src/components/banana.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>i am banana</h1>
+</template>
+
+<script>
+    export default {
+        name: 'banana'
+    }
+</script>
